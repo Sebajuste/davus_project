4c44e6147d8b72b87efd33a6682ec850a6b20e7d
Nono02P <nono02p@hotmail.fr>
2019-09-02 02:49:14 +0200

    Gestion des tuiles par catégories qui peuvent chacunes contenir plusieurs variantes : - 1 => 1 côté solide (orienté en haut) - 2 => 2 côtés solides (orientés en haut et à droite) - 3 => 3 côtés solides (orientés en haut, à droite et en bas) - 4 => 4 côtés solides - 5 => 2 côtés solides (orientés en face à face, en haut et en bas)
    La catégorie et l'angle sont déterminés par le code en fonction de la valeur du masque (1 à F. La valeur 0 étant une tuile vide).
    
    Ajout du fichier excel expliquant le principe du bitmasking employé.

--- tools/LevelBatch.gd
+++ tools/LevelBatch.gd
@@ -1,31 +1,40 @@
 extends Spatial
 
-
 export var size := 16
-
 export(float, -1.0, 1.0) var cap := 0.15
-
 export var debug := true
 
 var _noise
-const TILES_RESOURCES := {
-    0x01: preload("res://models/World/Test/Tile01.glb"), # Top tile
-    0x02: preload("res://models/World/Test/Tile02.glb"), # Right tile
-    0x03: preload("res://models/World/Test/Tile03.glb"), # Top & Right tile
-    0x04: preload("res://models/World/Test/Tile04.glb"), # Bottom tile
-    0x05: preload("res://models/World/Test/Tile05.glb"), # Top & Bottom tile
-    0x06: preload("res://models/World/Test/Tile06.glb"), # Right & Bottom tile
-    0x07: preload("res://models/World/Test/Tile07.glb"), # Top & Right & Bottom tile
-    0x08: preload("res://models/World/Test/Tile08.glb"), # Left tile
-    0x09: preload("res://models/World/Test/Tile09.glb"), # Top & Left tile
-    0x0A: preload("res://models/World/Test/Tile10.glb"), # Right & Left tile
-    0x0B: preload("res://models/World/Test/Tile11.glb"), # Top & Right & Left tile
-    0x0C: preload("res://models/World/Test/Tile12.glb"), # Bottom & Left tile
-    0x0D: preload("res://models/World/Test/Tile13.glb"), # Top & Bottom & Left tile
-    0x0E: preload("res://models/World/Test/Tile14.glb"), # Right & Bottom & Left tile
-    0x0F: preload("res://models/World/Test/Tile15.glb"), # Top & Right & Bottom & Left tile
-    0x10: preload("res://models/World/Test/Tile00.glb"), # Full tile
-}
+
+ # TODO : Complete the categories with the corresponding files.
+const TILES_RESOURCES := { 
+	0x01: 
+		[ # Category 1 
+	    	preload("res://models/World/Jungle/Tiles/Tile00.glb"),
+	    	preload("res://models/World/Jungle/Tiles/Tile01.glb"),
+		],
+	0x02: 
+		[ # Category 2
+			preload("res://models/World/Jungle/Tiles/Tile00.glb"),
+			preload("res://models/World/Jungle/Tiles/Tile01.glb"),
+		],
+	0x03: 
+		[ # Category 3
+			preload("res://models/World/Jungle/Tiles/Tile00.glb"),
+			preload("res://models/World/Jungle/Tiles/Tile01.glb"),
+		],
+	0x04: 
+		[ # Category 4
+			preload("res://models/World/Jungle/Tiles/Tile00.glb"),
+			preload("res://models/World/Jungle/Tiles/Tile01.glb"),
+		],
+	0x05: 
+		[ # Category 5
+			preload("res://models/World/Jungle/Tiles/Tile00.glb"),
+			preload("res://models/World/Jungle/Tiles/Tile01.glb"),
+		],
+	}
+
 
 func _ready():
 	
@@ -48,20 +57,18 @@ func gen(loc: Vector3):
 	
 	for x in range(size):
 		for y in range(size):
-			
 			var pos = Vector3(loc.x+x, loc.y-y, 0)
-			
-			var value = _noise.get_noise_2d(pos.x, pos.y)
 			var mask = 0
-			if value <= cap:
+			if _is_solid_tile(pos.x, pos.y):
 				mask = _create_bitmask( pos )
-			else:
-				mask = 0x10
-			
-			var tile = _create_tile(mask)
-			if tile:
-				self.add_child(tile)
-				tile.translate( Vector3(x*2, (size*2)-(y*2), 0) )
+				var tile = _create_tile(mask)
+				if tile:
+					self.add_child(tile)
+					tile.translate( Vector3(x*2, (size*2)-(y*2), 0) )
+
+
+func _is_solid_tile(x: int, y: int):
+	return _noise.get_noise_2d(x, y) > cap
 
 
 func _create_bitmask(pos: Vector3) -> int:
@@ -71,20 +78,63 @@ func _create_bitmask(pos: Vector3) -> int:
 	
 	for i in range(1, -2, -2):
 		# Top / Bottom
-		if _noise.get_noise_2d(pos.x, pos.y + i) > cap:
+		if not _is_solid_tile(pos.x, pos.y + i):
 			mask |= 0x01 << index
 		
 		# Left / Right
-		if _noise.get_noise_2d(pos.x + i, pos.y) > cap:
+		if not _is_solid_tile(pos.x + i, pos.y):
 			mask |= 0x02 << index
 			
 		index += 2
 		
 	return mask
 
-func _create_tile(mask: int) -> Node:
+
+func _get_tile_variant(category: int, angle: float) -> Node:
 	
-    if TILES_RESOURCES.has(mask):
-        return TILES_RESOURCES.get(mask).instance()
-    else:
-        return null
+	var tilesCategory = TILES_RESOURCES.get(category)
+	var variant = randi() % tilesCategory.size()
+	var tile = tilesCategory[variant].instance()
+	
+	if angle != 0:
+		# TODO : Check compliance with the "Tile" name in the tiles files.
+		tile.find_node("Tile").rotate_z(angle)
+	
+	return tile
+
+
+func _create_tile(mask: int) -> Node:
+
+	match mask:
+		0x1:	# Top
+			return _get_tile_variant(1, 0)
+		0x2:	# Right
+			return _get_tile_variant(1, PI * 0.5)
+		0x3:	# Top Right
+			return _get_tile_variant(2, 0)
+		0x4:	# Bottom
+			return _get_tile_variant(1, PI)
+		0x5:	# Top Bottom
+			return _get_tile_variant(5, 0)
+		0x6:	# Right Bottom
+			return _get_tile_variant(2, PI * 0.5)
+		0x7:	# Top Right Bottom
+			return _get_tile_variant(3, 0)
+		0x8:	# Left
+			return _get_tile_variant(1, PI * 1.5)
+		0x9:	# Top Left
+			return _get_tile_variant(2, PI * 1.5)
+		0xA:	# Right Left
+			return _get_tile_variant(5, PI * 0.5)
+		0xB:	# Top Right Left
+			return _get_tile_variant(3, PI * 1.5)
+		0xC:	# Bottom Left
+			return _get_tile_variant(2, PI)
+		0xD:	# Top Bottom Left
+			return _get_tile_variant(3, PI)
+		0xE:	# Right Bottom Left
+			return _get_tile_variant(3, PI * 0.5)
+		0xF:	# Top Right Bottom Left
+			return _get_tile_variant(4, 0)
+		_:	
+			return null
