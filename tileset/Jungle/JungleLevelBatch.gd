4c6a1e2fe5f0cc84b3a1081f1e77476a51dce66b
Sebajuste <sebajuste@hotmail.com>
2019-09-03 20:26:15 +0200

    Add heritage for LevelBatch, and biome selector in StreamingLevel

--- tileset/Jungle/JungleLevelBatch.gd
+++ tileset/Jungle/JungleLevelBatch.gd
@@ -1,10 +1,5 @@
-extends Spatial
+extends "res://tileset/LevelBatch.gd"
 
-export var size := 16
-export(float, -1.0, 1.0) var cap := 0.15
-export var debug := true
-
-var _noise
 
 const TILES_RESOURCES := { 
 	0x00:
@@ -89,52 +84,33 @@ const TILES_RESOURCES := {
 	}
 
 
-func _ready():
-	
-	if debug:
-		$ImmediateGeometry.begin(Mesh.PRIMITIVE_LINE_LOOP)
-		
-		$ImmediateGeometry.set_color(Color(1.0, 0.0, 0.0))
-		
-		$ImmediateGeometry.add_vertex(Vector3(0, 0, 1))
-		$ImmediateGeometry.add_vertex(Vector3(size*2, 0, 1))
-		$ImmediateGeometry.add_vertex(Vector3(size*2, size*2, 1))
-		$ImmediateGeometry.add_vertex(Vector3(0, size*2, 1))
-		
-		$ImmediateGeometry.end()
-	
-	pass
-
 
 func gen(loc: Vector3):
-	
 	for x in range(size):
 		for y in range(size):
-			var pos = Vector3(loc.x+x, loc.y-y, 0)
-			if _is_solid_tile(pos.x, pos.y):
-				var mask = _create_bitmask( pos )
+			var global_pos = Vector3(loc.x+x, loc.y-y, 0)
+			if _is_solid_tile(global_pos):
+				var mask = _create_bitmask( global_pos )
 				var tile = _create_tile(mask)
 				if tile:
 					self.add_child(tile)
 					tile.translate( Vector3(x*2, (size*2)-(y*2), 0) )
 
 
-func _is_solid_tile(x: int, y: int):
-	return _noise.get_noise_2d(x, y) > cap
-
-
-func _create_bitmask(pos: Vector3) -> int:
+func _create_bitmask(global_pos: Vector3) -> int:
 	
 	var mask = 0x00
 	var index = 0
 	
 	for i in range(1, -2, -2):
 		# Top / Bottom
-		if not _is_solid_tile(pos.x, pos.y + i):
+		var top_bottom_global_pos = global_pos + Vector3(0, i, 0)
+		if not _is_solid_tile(top_bottom_global_pos):
 			mask |= 0x01 << index
 		
 		# Left / Right
-		if not _is_solid_tile(pos.x + i, pos.y):
+		var left_right_global_pos = global_pos + Vector3(i, 0, 0)
+		if not _is_solid_tile(left_right_global_pos):
 			mask |= 0x02 << index
 			
 		index += 2
@@ -143,7 +119,6 @@ func _create_bitmask(pos: Vector3) -> int:
 
 
 func _create_tile(mask: int) -> Node:
-
 	var tilesCategory = TILES_RESOURCES.get(mask)
 	var variant = randi() % tilesCategory.size()
 	return tilesCategory[variant].instance()
