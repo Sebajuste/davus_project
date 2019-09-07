2f32329f942e1e46ee46b2208c4995aa68f82c9e
Nono02P <nono02p@hotmail.fr>
2019-09-07 14:48:33 +0200

    Passage d'une map à plat sur les axes x, z vers une map "debout" sur les axes x, y. Mise en place d'un paramètre extérieur pour régler la seed. Inversion de l'axe Y pour l'affichage des rectangles 2D à l'identique de la vue 3D (pour gérer une future minimap).

--- tileset/DungeonGenerator/Dungeon.gd
+++ tileset/DungeonGenerator/Dungeon.gd
@@ -4,4 +4,4 @@ func _on_DungeonGenerator_graph_gen_finnished():
 	var dg = $DungeonGenerator
 	dg.generate_grid_map($GridMap)
 	$Camera.translation.x = dg.map_width * dg.tile_size
-	$Camera.translation.z = dg.map_height * dg.tile_size
+	$Camera.translation.y = dg.map_height * dg.tile_size
--- tileset/DungeonGenerator/DungeonGenerator.gd
+++ tileset/DungeonGenerator/DungeonGenerator.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+export var scale2D = 2
 export var tile_size = 2
 export var room_margin = 1
-export(int, 3, 13) var number_of_rooms = 5 
+export(int, 3, 13) var number_of_rooms = 5
 export var map_width = 40
 export var map_height = 25
 export var min_room_size = 5
 export var max_room_size = 10
+export var map_seed = 2
 
 signal graph_gen_finnished
 
@@ -14,9 +16,10 @@ enum eTilesType { Empty = -1, Floor = 0, Wall = 1 }
 
 var rooms_areas = Array()
 var pathfinding := AStar.new()
-
+var rnd = RandomNumberGenerator.new()
 
 func _ready():
+	rnd.seed = map_seed
 	gen_graph()
 
 func _input(event):
@@ -50,18 +53,16 @@ func generate_rooms() -> Array:
 	return locations
 
 func create_room() -> Rect2:
-	var width = (min_room_size + randi() % (max_room_size - min_room_size)) * tile_size
-	var height = (min_room_size + randi() % (max_room_size - min_room_size)) * tile_size
-	var x = randi() % (map_width * tile_size - width)
-	var y = randi() % (map_height * tile_size - height)
+	var width = (min_room_size + rnd.randi() % (max_room_size - min_room_size)) * tile_size
+	var height = (min_room_size + rnd.randi() % (max_room_size - min_room_size)) * tile_size
+	var x = rnd.randi() % (map_width * tile_size - width)
+	var y = rnd.randi() % (map_height * tile_size - height)
 	var pos = Vector2(x - (x % tile_size), y - (y % tile_size))
 	var size = Vector2(width, height)
 	return Rect2(pos, size)
 
 func get_room_rectangle(area: Rect2) -> Rect2:
 	return area.grow(- room_margin * tile_size)
-	#var margin = Vector2.ONE * room_margin * tile_size
-	#return Rect2(area.position + margin, area.size - margin * 2)
 
 func generate_graph(locations: Array) -> AStar:
 	var astar = AStar.new()
@@ -89,14 +90,14 @@ func generate_graph(locations: Array) -> AStar:
 
 func generate_grid_map(map:GridMap):
 	map.clear()
-	write_floor_on_map(map)
+	fill_the_map(map)
 	write_rooms_on_map(map)
 	write_corridors_on_map(map)
 
-func write_floor_on_map(map: GridMap):
+func fill_the_map(map: GridMap):
 	for x in range(map_width * tile_size):
-		for z in range(map_height * tile_size):
-			map.set_cell_item(x, -1, z, eTilesType.Floor)
+		for y in range(map_height * tile_size):
+			map.set_cell_item(x, y, 0, eTilesType.Wall)
 
 func write_rooms_on_map(map: GridMap):
 	for area in rooms_areas:
@@ -105,12 +106,13 @@ func write_rooms_on_map(map: GridMap):
 		var right = left + room.size.x
 		var top = room.position.y
 		var bottom = top + room.size.y
+		var z = 0
 		for x in range(left, right + 1):
-			for z in range(top, bottom + 1):
-				map.set_cell_item(x, 0, z, 1)	# room floor
-				if x == left or x == right or z == top or z == bottom:
-					map.set_cell_item(x, 1, z, eTilesType.Wall)	# room walls
-		
+			for y in range(top, bottom + 1):
+				map.set_cell_item(x, y, z, eTilesType.Empty)	# room floor
+				if x == left or x == right or y == top or y == bottom:
+					map.set_cell_item(x, y, z, eTilesType.Empty)	# room walls
+
 
 func write_corridors_on_map(map: GridMap):
 	for area in rooms_areas:
@@ -123,41 +125,51 @@ func write_corridors_on_map(map: GridMap):
 				dig_path(map, start, end)
 				corridors.append(connection)
 
+
 func dig_path(map: GridMap, start: Vector3, end: Vector3):
 	var xStep = sign(end.x - start.x)
-	var zStep = sign(end.z - start.z)
-	if xStep == 0: xStep = pow(-1, randi() % 2)
-	if zStep == 0: zStep = pow(-1, randi() % 2)
-	var x_to_z = start		# Direction horizontal and vertical
-	var z_to_x = end		# Direction vertical and horizontal
-	if randi() % 2 == 0:
-		z_to_x = start
-		x_to_z = end
+	var yStep = sign(end.y - start.y)
+	if xStep == 0: xStep = pow(-1, rnd.randi() % 2)
+	if yStep == 0: yStep = pow(-1, rnd.randi() % 2)
+	var x_to_y = start		# Direction horizontal and vertical
+	var y_to_x = end		# Direction vertical and horizontal
+	var z = 0
+	if rnd.randi() % 2 == 0:
+		y_to_x = start
+		x_to_y = end
 	for x in range(start.x, end.x + xStep, xStep): # + xStep * 2, xStep):
-		map.set_cell_item(x, 1, x_to_z.z, eTilesType.Empty)
-		#map.set_cell_item(x, 0, x_to_z.z - zStep, eTilesType.Wall)
-		map.set_cell_item(x, 0, x_to_z.z, eTilesType.Wall)
-		#map.set_cell_item(x, 0, x_to_z.z + zStep, eTilesType.Wall)
-	for z in range(start.z, end.z + zStep, zStep): # + zStep * 2, zStep):
-		map.set_cell_item(z_to_x.x, 1, z, eTilesType.Empty)
-		#map.set_cell_item(z_to_x.x - xStep, 0, z, eTilesType.Wall)
-		map.set_cell_item(z_to_x.x, 0, z, eTilesType.Wall)
-		#map.set_cell_item(z_to_x.x + xStep, 0, z, eTilesType.Wall)
+		#map.set_cell_item(x, x_to_y.y - yStep, z, eTilesType.Empty)
+		map.set_cell_item(x, x_to_y.y, z, eTilesType.Empty)
+		map.set_cell_item(x, x_to_y.y + yStep, z, eTilesType.Empty)
+	for y in range(start.y, end.y + yStep, yStep): # + yStep * 2, yStep):
+		#map.set_cell_item(y_to_x.x - xStep, y, z, eTilesType.Empty)
+		map.set_cell_item(y_to_x.x, y, z, eTilesType.Empty)
+		map.set_cell_item(y_to_x.x + xStep, y, z, eTilesType.Empty)
 
 
-func to_vector3(v: Vector2, y :int = 0) -> Vector3:
-	return Vector3(v.x, y, v.y)
+func to_vector3(v: Vector2, z :int = 0) -> Vector3:
+	return Vector3(v.x, v.y, z)
 	
 func to_vector2(v: Vector3) -> Vector2:
-	return Vector2(v.x, v.z)
+		return Vector2(v.x, v.y)
+
+func reverse_y_axis(v: Vector2):
+	return Vector2(v.x, map_height * tile_size -v.y) 
 
 func get_middle(r: Rect2) -> Vector3:
 	return to_vector3(r.position + (r.size / 2))
 
+func scale_rectangle(r: Rect2, scale: int, reverseY: bool = false) -> Rect2:
+		if reverseY:
+			var pos = Vector2(r.position.x, r.position.y + r.size.y - map_height * tile_size / 2)
+			return Rect2(reverse_y_axis(pos * scale), r.size * scale)
+		else:
+			return Rect2(r.position * scale, r.size * scale)
+
 func _draw():
 	for area in rooms_areas:
-		draw_rect(area, Color.white, false)						# draw area
-		draw_rect(get_room_rectangle(area), Color.red, true)	# draw room (without margin)
+		draw_rect(scale_rectangle(area, scale2D, true), Color.white, false)						# draw area
+		draw_rect(scale_rectangle(get_room_rectangle(area), scale2D, true), Color.red, true)	# draw room (without margin)
 	draw_path(pathfinding)
 
 func draw_path(path: AStar):
@@ -166,4 +178,4 @@ func draw_path(path: AStar):
 			for edges in path.get_point_connections(point):
 				var pointPosition = path.get_point_position(point)
 				var edgePosition = path.get_point_position(edges)
-				draw_line(to_vector2(pointPosition), to_vector2(edgePosition), Color.yellow)
+				draw_line(reverse_y_axis(to_vector2(pointPosition)) * scale2D, reverse_y_axis(to_vector2(edgePosition)) * scale2D, Color.yellow)
