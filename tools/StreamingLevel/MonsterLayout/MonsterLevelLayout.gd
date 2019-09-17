0d255e0694753eb7157b826d6af211865c82b105
Sebajuste <sebajuste@hotmail.com>
2019-09-17 07:40:19 +0200

    change variable names

--- tools/StreamingLevel/MonsterLayout/MonsterLevelLayout.gd
+++ tools/StreamingLevel/MonsterLayout/MonsterLevelLayout.gd
@@ -4,15 +4,15 @@ const MonsterLayoutBatch = preload("MonsterLayoutBatch.tscn")
 
 const TILE_SIZE := 2
 
-export var top_y := -2
-export var top_cap := 0.0
-export var end_y := 0
+export var top_max_y := -2
+export var top_max_value := 0.0
+export var end_max_y := 0
 
 func gen(pos: Vector3) -> Spatial:
 	var layout_batch = MonsterLayoutBatch.instance()
-	layout_batch.top_y = top_y
-	layout_batch.top_cap = top_cap
-	layout_batch.end_y = end_y
+	layout_batch.top_max_y = top_max_y
+	layout_batch.top_max_value = top_max_value
+	layout_batch.end_max_y = end_max_y
 	layout_batch.translate( Vector3(pos.x*batch_size*TILE_SIZE, pos.y*batch_size*TILE_SIZE, 0) )
 	layout_batch.gen( Vector3(pos.x*batch_size, pos.y*batch_size, 0), noise, cap )
 	return layout_batch
