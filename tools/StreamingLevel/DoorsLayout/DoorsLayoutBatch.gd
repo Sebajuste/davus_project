4fc0e3e3f315fbdc2962c056f1823c1c4d8d3265
Sebajuste <sebajuste@hotmail.com>
2019-09-20 22:39:22 +0200

    Add layout to create dungeon doors in world

---
+++ tools/StreamingLevel/DoorsLayout/DoorsLayoutBatch.gd
@@ -0,0 +1,22 @@
+extends "res://tools/StreamingLevel/LayoutBatch.gd"
+
+const DungeonEntrance = preload("res://objects/doors/DungeonEntrance/DungeonEntrance.tscn")
+
+var top_max_y : int
+var top_max_value : float
+var end_max_y : int
+
+func gen(loc: Vector3, noise: OpenSimplexNoise, cap: float) -> void:
+	seed(str(loc).hash())
+	for x in range(size):
+		for y in range(size):
+			var global_pos = Vector3(loc.x+x, loc.y-y, 0)
+			if global_pos.y < end_max_y and _is_solid_tile(global_pos, noise, cap) and not _is_solid_tile(global_pos+Vector3(0, 1, 0), noise, cap) and randi() % 32 == 1:
+				var dungeon_entrance = DungeonEntrance.instance()
+				self.add_child(dungeon_entrance)
+				dungeon_entrance.locked = true
+				dungeon_entrance.translate( Vector3(x*2, (size*2)-(y*2), -2) )
+
+
+func _is_solid_tile(global_pos: Vector3, noise: OpenSimplexNoise, cap: float) -> bool:
+	return noise.get_noise_2d(global_pos.x, global_pos.y) > cap
---
+++ tools/StreamingLevel/DoorsLayout/DoorsLevelLayout.gd
@@ -0,0 +1,18 @@
+extends "res://tools/StreamingLevel/LevelLayout.gd"
+
+const DoorsLayoutBatch = preload("DoorsLayoutBatch.tscn")
+
+const TILE_SIZE := 2
+
+export var top_max_y := -2
+export(float, -1.0, 1.0) var top_max_value := 0.0
+export var end_max_y := 0
+
+func gen(pos: Vector3) -> Spatial:
+	var layout_batch = DoorsLayoutBatch.instance()
+	layout_batch.top_max_y = top_max_y
+	layout_batch.top_max_value = top_max_value
+	layout_batch.end_max_y = end_max_y
+	layout_batch.translate( Vector3(pos.x*batch_size*TILE_SIZE, pos.y*batch_size*TILE_SIZE, 0) )
+	layout_batch.gen( Vector3(pos.x*batch_size, pos.y*batch_size, 0), noise, cap )
+	return layout_batch
