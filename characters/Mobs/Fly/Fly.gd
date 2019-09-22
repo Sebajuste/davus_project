eb262493b9a413079ee169a850ce064a8536b0fd
Z0hung44r <z0hung44r@gmail.com>
2019-09-22 10:56:33 +0200

    Les mouches peuvent attaquer le joueur

--- characters/Mobs/Fly/Fly.gd
+++ characters/Mobs/Fly/Fly.gd
@@ -2,18 +2,26 @@ extends "res://characters/Mobs/Mob.gd"
 
 const Bullet = preload("res://objects/projectiles/Bullet/Bullet.tscn")
 
+export(float) var distance_min = 2.5 # Distance minimum qu'il doit y avoir entre le joueur et la mouche lorsque la mouche attaque
+export(float) var distance_max = 3.0 # Distance maximum qu'il doit y avoir entre le joueur et la mouche lorsque la mouche attaque
+export(float) var speed = 3.0 # Vitesse de la mouche
+export(float) var attack_rate = 1.0 # Nombre de tirs de la mouche par secondes
+
 enum state { 
 	CHOOSE_TARGET, 
 	MOVE_FORWARD, 
 	MOVE_BACKWARD,
-	WAIT
+	ATTACK
 }
+
+const OFFSET_Y = 1 # Equivaut à la moitié de la hauteur du joueur
 
 var anim_state_machine
 var current_state
-var position = global_transform.origin
-var vx = 0
-var vy = 0
+var position = global_transform.origin # position de la mouche
+var velocity = Vector2(0,0)
+var attack_speed = 1 / attack_rate # Temps de "rechargement" de la mouche (en secondes) / calculé en fonction du Nombre de tirs par secondes
+var attack_timer = 0
 
 # Returns the angle between two points.
 func get_angle(x1,y1,x2,y2) -> float:
@@ -40,7 +48,15 @@ func _process(delta):
 	if $CombatStats.health == 0:
 		return
 	
-	position = global_transform.origin
+	position = global_transform.origin # Position de la mouche
+	var target_position
+	var dist
+	
+	if current_target != null:
+		target_position = Vector2(current_target.global_transform.origin.x, current_target.global_transform.origin.y + OFFSET_Y)
+		dist = get_dist(position.x, position.y, target_position.x, target_position.y)
+	else:
+		current_state = state.CHOOSE_TARGET
 	
 	# AI
 	if current_state == state.CHOOSE_TARGET:
@@ -51,40 +67,41 @@ func _process(delta):
 		
 	elif current_state == state.MOVE_FORWARD:
 		
-		var target_position = current_target.global_transform.origin
 		var angle = get_angle(position.x, position.y, target_position.x, target_position.y)
-		vx = cos(angle) * 2
-		vy = sin(angle) * 2
+		velocity = Vector2(cos(angle) * speed, sin(angle) * speed)
 		
-		var dist = get_dist(position.x, position.y, target_position.x, target_position.y)
-		if abs(dist) <= 3:
-			current_state = state.WAIT
-	
-	elif current_state == state.WAIT:
+		if abs(dist) <= distance_max:
+			current_state = state.ATTACK
+			attack_timer = attack_speed
 		
-		vx = 0
-		vy = 0
+	elif current_state == state.ATTACK:
 		
-		var target_position = current_target.global_transform.origin
-		var dist = get_dist(position.x, position.y, target_position.x, target_position.y)
-		if abs(dist) > 3:
+		attack_timer -= delta
+		if attack_timer <= 0:
+			if anim_state_machine.is_playing():
+				anim_state_machine.travel("Attack")
+			else:
+				anim_state_machine.start("Attack")
+			
+			attack_timer = attack_speed
+			attack()
+		
+		velocity = Vector2.ZERO
+		
+		if abs(dist) > distance_max:
 			current_state = state.MOVE_FORWARD
-		elif abs(dist) <= 2.5:
+		elif abs(dist) <= distance_min:
 			current_state = state.MOVE_BACKWARD
-		
 		
 	elif current_state == state.MOVE_BACKWARD:
 		
-		var target_position = current_target.global_transform.origin
 		var angle = get_angle(position.x, position.y, target_position.x, target_position.y)
-		vx = -(cos(angle) * 3)
-		vy = -(sin(angle) * 3)
+		velocity = Vector2(-(cos(angle) * speed), -(sin(angle) * speed))
 		
-		var dist = get_dist(position.x, position.y, target_position.x, target_position.y)
-		if abs(dist) > 2.5:
-			current_state = state.WAIT
+		if abs(dist) > distance_min:
+			current_state = state.ATTACK
 	
-	move_and_slide(Vector3(vx, vy, 0))
+	move_and_slide(Vector3(velocity.x, velocity.y, 0))
 
 
 func _physics_process(delta):
@@ -106,7 +123,17 @@ func attack():
 	root_node.add_child(bullet)
 	
 	bullet.global_transform.origin = $AttachPosition.global_transform.origin
-	bullet.direction = (current_target.global_transform.origin - self.global_transform.origin).normalized()
+	
+	# Modification des positions utilisées pour diriger le tir
+	var target_position = current_target.global_transform.origin
+	var new_position_target = Vector3(target_position.x, target_position.y + OFFSET_Y, target_position.z)
+	var new_position # de la mouche
+	if target_position.x > position.x:
+		new_position = Vector3(self.global_transform.origin.x + 1, self.global_transform.origin.y, 0)
+	else:
+		new_position = Vector3(self.global_transform.origin.x - 1, self.global_transform.origin.y, 0)
+		
+	bullet.direction = (new_position_target - new_position).normalized()
 
 
 
