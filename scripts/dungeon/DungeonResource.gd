f52fa7faa1ae5df0d72f304eb45de29199a485ec
Nono02P <nono02p@hotmail.fr>
2019-09-25 23:42:58 +0200

    Close #119, Fix ##168 Repositionnement des mobs au bon endroit en fonction de leur type (fly/floor) afin de ne plus avoir de plantes qui ne soient pas au sol. Différenciation des décors de salles (extrémités (start/end) ou normal). Mise au propre du dossier de tileset/test/ Ajout de la tuile définitive pour le mur plein. Fermeture de la salle de fin au moment de sa création.

--- scripts/dungeon/DungeonResource.gd
+++ scripts/dungeon/DungeonResource.gd
@@ -1,24 +1,37 @@
 class_name DungeonResource
 
+# Tiles
 enum eTilesType { 
-		Empty = -1, 
-		Door = 0, 
-		Wall = 1, 
-		Ladder = 2,   
+		Empty = -1,
+		Door = 0,
+		Wall = 1,
+		Ladder = 2,
 		PipeStraight = 3,
 		PipeTurn = 4,
 		# DEBUG :
-			Key = 5, 
-			Start = 6, 
-			End = 7, 
-			DoorInsertion = 8, 
+			DoorInsertion = 5,
 	}
+	
 const STATIC_BODIES:Dictionary = { 
-		eTilesType.Door: preload("res://tileset/test/Door.tscn"),
-		eTilesType.Wall: preload("res://tileset/test/Wall.tscn"),
-		eTilesType.Ladder: preload("res://tileset/test/RightLadder.tscn"),
+		eTilesType.Wall: preload("res://tileset/Dungeon/Wall.tscn"),
+		eTilesType.Ladder: preload("res://tileset/Dungeon/RightLadder.tscn"),
 	}
+	
+# Doors
+const IN_OUT_DOOR:Array = [
+		preload("res://objects/doors/DungeonExit/DungeonExit.tscn")
+	]
 
+const INTERIOR_DOORS:Array = [
+		preload("res://objects/doors/DungeonPipe/DungeonPipe00.tscn"),
+	]
+
+# Keys
+const KEYS_RESOURCES:Array = [
+		preload("res://objects/keys/DoorKey/DoorKey.tscn"),
+	]
+
+# Rooms
 const ROOM_PREFAB:Dictionary = {
 	9:
 		{
@@ -29,11 +42,19 @@ const ROOM_PREFAB:Dictionary = {
 		},
 	}
 
-const ROOM_BACKGROUND:Dictionary = {
+const EXTREMITIES_ROOM_BACKGROUND:Dictionary = {
 	9:
 		{
 			5: [
 				preload("res://models/Rooms/room00.glb"),
+			],
+		},
+	}
+
+const ROOM_BACKGROUND:Dictionary = {
+	9:
+		{
+			5: [
 				preload("res://models/Rooms/room01.glb"),
 				preload("res://models/Rooms/room02.glb"),
 				preload("res://models/Rooms/room03.glb"),
@@ -42,20 +63,15 @@ const ROOM_BACKGROUND:Dictionary = {
 		},
 	}
 
-const KEYS_RESOURCES:Array = [
-		preload("res://objects/keys/DoorKey/DoorKey.tscn"),
-	]
+# Mobs
+enum eMobType { Floor = 0, Fly = 1 }
+const MOB_RESOURCES:Dictionary = {
+		eMobType.Floor: [
+			preload("res://characters/Mobs/MobTentacle/MobTentacle.tscn"),
+		],
+		eMobType.Fly: [
+			preload("res://characters/Mobs/Drone/Drone.tscn"),
+			preload("res://characters/Mobs/Fly/Fly.tscn"),
+		],
+	}
 
-const MOB_RESOURCES:Array = [
-		preload("res://characters/Mobs/MobTentacle/MobTentacle.tscn"),
-		#preload("res://characters/Mobs/Drone/Drone.tscn"),
-		preload("res://characters/Mobs/Fly/Fly.tscn"),
-	]
-
-const IN_OUT_DOOR:Array = [
-		preload("res://objects/doors/DungeonExit/DungeonExit.tscn")
-	]
-	
-const INTERIOR_DOORS:Array = [
-		preload("res://objects/doors/DungeonPipe/DungeonPipe00.tscn"),
-	]
