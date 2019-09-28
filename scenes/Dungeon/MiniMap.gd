fc9b550d4c1d1f8e6670e0d608940c107d78df00
Nono02P <nono02p@hotmail.fr>
2019-09-28 22:16:36 +0200

    Ajout d'un rond en guise de position du joueur sur la minimap à la place de la coloration de salles.

--- scenes/Dungeon/MiniMap.gd
+++ scenes/Dungeon/MiniMap.gd
@@ -10,7 +10,7 @@ var graph_generator:GraphGenerator setget set_graph_generator
 var player_position:Vector3
 
 func gen(playerPos:Vector3):
-	player_position = playerPos
+	player_position = _geometry.vector3_round(playerPos)
 	update()
 
 func set_graph_generator(v):
@@ -25,14 +25,23 @@ func _draw():
 	var scaledMap:Vector2 = Vector2(graph_generator.map_width * scale2D, graph_generator.map_height * scale2D)
 	var offset:Vector2 = (viewportSize - scaledMap) * 0.5
 	var actual_room = graph_generator.pathfinding.get_closest_point(player_position)
+	var should_draw_hero:bool = true
 	for point in graph_generator.rooms_areas.keys():
 		var room:Room = graph_generator.rooms_areas[point]
 		var room_rect:Rect2 = room.get_room_rect()
 		
 		var color = Color.white
 		if actual_room == point && room_rect.has_point(_geometry.vector2_ceil(_geometry.to_vector2(player_position))):
-				color = Color.blue
-		elif room == graph_generator.starting_room:
+			#color = Color.blue
+			should_draw_hero = false
+			draw_circle(
+				offset + _geometry.reverse_y_axis(
+						_geometry.to_vector2(player_position), 
+						graph_generator.map_height
+						) * scale2D, 
+					5, 
+					Color.blue)
+		if room == graph_generator.starting_room:
 			color = Color.green
 		elif room == graph_generator.ending_room:
 			color = Color.red
@@ -56,15 +65,15 @@ func _draw():
 				str(point), 
 				Color.red)
 	_draw_path(graph_generator.pathfinding, offset)
-	"""
-	draw_circle(
-		offset + _geometry.reverse_y_axis(
-				_geometry.to_vector2(player_position - Vector3.DOWN), 
-				graph_generator.map_height
-				) * scale2D, 
-			5, 
-			Color.blue)
-	"""
+	if should_draw_hero:
+		var pos:Vector3 = graph_generator.pathfinding.get_closest_position_in_segment(player_position)
+		draw_circle(
+				offset + _geometry.reverse_y_axis(
+						_geometry.to_vector2(pos), 
+						graph_generator.map_height
+						) * scale2D, 
+					5, 
+					Color.blue)
 
 func _draw_path(path: AStar, offset:Vector2):
 	if path:
@@ -83,4 +92,3 @@ func _draw_path(path: AStar, offset:Vector2):
 					) * scale2D
 				
 				draw_line(scaledPointPos, scaledConnectionPos, Color.yellow)
-
