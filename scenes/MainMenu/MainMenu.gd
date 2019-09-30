d46d94e415f7e64dc688632a6263ef9cda8588bd
Sebajuste <sebajuste@hotmail.com>
2019-09-30 20:17:46 +0200

    Create cinematic scene introduction

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -9,15 +9,41 @@ func _ready():
 	$Spatial/FemaleCharacter/AnimationPlayer.play("angry")
 	$MainControls/MarginContainer/VBoxContainer/StartButton.grab_focus()
 	
+	set_alarm(false)
+	set_console(false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
-func _on_StartButton_pressed():
+
+func set_alarm(value: bool):
+	var mat = $Spatial/SpaceShip/ShipInterior/Gyro/GyroClass.mesh.surface_get_material(0)
+	mat.emission_enabled = value
+	$Spatial/AlarmLight.visible = value
+	if value:
+		$AlarmAnimationPlayer.play("alarm")
+	else:
+		$AlarmAnimationPlayer.stop()
+
+
+func set_console(value: bool):
+	var mat = $Spatial/Console/Datapad.get_surface_material(1)
+	mat.emission_enabled = value
+	
+
+
+func start_game():
 	
 	loading.change_scene("res://scenes/MainGame/MainGame.tscn")
+	
+
+
+
+func _on_StartButton_pressed():
+	$MainControls.visible = false
+	$CinematicAnimationPlayer.play("intro")
 	
 
 
