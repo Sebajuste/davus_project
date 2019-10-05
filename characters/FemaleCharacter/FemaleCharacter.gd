85ea326808b660414b4fa4f9df27ae794b9db1a1
Sebajuste <sebajuste@hotmail.com>
2019-10-05 08:47:42 +0200

    Laser pointer display correction

--- characters/FemaleCharacter/FemaleCharacter.gd
+++ characters/FemaleCharacter/FemaleCharacter.gd
@@ -91,11 +91,12 @@ func _process(delta):
 
 func _physics_process(delta):
 	
-	
 	if $WeaponHandler.aiming and $WeaponHandler.valid_target and $WeaponHandler.shoot_ready and controller.type == controller.Type.GAMEPAD:
-		$LaserPointer.visible = true
-		$LaserPointer.weapon_pos = $WeaponHandler.get_weapon().global_transform.origin
-		$LaserPointer.target_pos = $WeaponHandler.target
+		var weapon = $WeaponHandler.get_weapon()
+		if weapon != null:
+			$LaserPointer.visible = true
+			$LaserPointer.weapon_pos = weapon.global_transform.origin
+			$LaserPointer.target_pos = $WeaponHandler.target
 	else:
 		$LaserPointer.visible = false
 	
